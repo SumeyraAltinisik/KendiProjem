ab646ae94ee1912856abaad564e82a2215beee86
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-05 17:55:02 +0300

    Add project files.

---
+++ KendiProjem/Controllers/ArtistController.cs
@@ -0,0 +1,36 @@
+﻿using KendiProjem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KendiProjem.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public IActionResult Index()
+        {
+            ChinookContext cnt = new ChinookContext();
+            return View(cnt.Artists.ToList());
+        }
+
+        public IActionResult AlbumsByArtist(int id)
+        {
+            ChinookContext cnt = new ChinookContext();
+            List<Album> albums = cnt.Albums.Where(x => x.ArtistId == id).ToList();
+            return View(albums);
+        }
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Create(Artist artist)
+        {
+            ChinookContext context = new ChinookContext();
+            context.Artists.Add(artist);
+            context.SaveChanges();
+            return View("Thanks", artist);
+        }
+    }
+}
